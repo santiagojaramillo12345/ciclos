137370f853037953117c219642c9d38015342572
alexgz23 <aleton2312@gmail.com>
2023-08-05 11:44:45 -0500

    acabar

---
+++ Ciclos.py
@@ -0,0 +1,8 @@
+#Estruturas de conmtrol Repetitivasd
+
+Pares=[2,4,6,8,10]
+suma = Pares[0] + Pares[2] + Pares[3] + Pares[4] 
+print(suma)
+
+print(" ")
+
---
+++ while2.py
@@ -0,0 +1,40 @@
+#pedir si un numero ingresado por el usuario es par o impar
+
+numero = int(input("por favor ingrese un numero entero positivo"))
+
+while numero >=0:
+    if numero % 2 ==0:
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+    break
+#imprimir los elementos de la lista que sean multiplos de 3
+
+valores = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+re=0
+while re>=len(valores):
+   if valores [re] %3== 0:
+        print(valores[re])
+re += 1
+
+
+
+"""
+
+Solicitar al usuario una contraeña y la confirmacion de esta. si las dos coniciden
+imprimir biebenido(a) de lo contrario mostrar usuario bloqueado
+numero de intentos disponibles y validar las mayusculas
+"""
+
+
+
+
+
+
+     
+    
+
+
+
+
